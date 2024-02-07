783cdfd968d4f11fa0c2d1a13c5dd62170d36eb8
yihedeng9 <yihedeng@cs.ucla.edu>
2024-02-06 16:29:45 -0800

    structuring

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# SPIN
-Self-Play Fine-Tuning
+# Self-Play Fine-Tuning (SPIN)
+
+This official repo holds code of the paper "[Self-Play Fine-Tuning Converts Weak Language Models to Strong Language Models](https://arxiv.org/abs/2401.01335)".
+
+Authors: [Zixiang Chen](https://sites.google.com/view/zxchen)\*, [Yihe Deng](https://sites.google.com/g.ucla.edu/yihedeng/)\*, [Huizhuo Yuan](https://scholar.google.com/citations?user=8foZzX4AAAAJ)\*, [Kaixuan Ji](https://scholar.google.com/citations?user=FOoKDukAAAAJ), [Quanquan Gu](https://web.cs.ucla.edu/~qgu/)
+
+<p align="center">
+    <img src="images/iter_openllm.png" width="50%"> <br>
+  Average score of <b>SPIN</b> at different iterations on the HuggingFace Open LLM leaderboard. 
+</p>
+
+## 🔔 News 
+- **[01/02/2024]** Our paper is released on arXiv: https://arxiv.org/abs/2401.01335.
+
+## Setup
+Install the following Python dependencies to reproduce our results.
+
+**[TODO]** Instructions for set-up.
+
+### Data 
+We provide the data used in our experiments along with the synthetic data we generated in this repo as well as on HuggingFace. These data is converted to .parquet format for fine-tuning (e.g. [iter0](data/iter0/train_prefs-00000-of-00001.parquet)).
+
+
+## Usage
+### Step 1: Generation
+```
+accelerate launch spin/generate.py [options]
+```
+Options
+- `--model`: load model checkpoint for generation.
+    - default: `alignment-handbook/zephyr-7b-sft-full`
+- `--input_dir`: directory to the data files with prompts for generation
+    - The code is for generation based on old data. 
+    - default: `synthetic_ultra_14k`
+- `--output_dir`: directory to save the output data. 
+- `--batch_size`: per device batch size
+    - default: 16
+- `--data_frac`: break data into fractions for generations across server.
+    - `--frac_len`: length of the data fraction. Default is 0 which uses the entire dataset for generation. Set `frac_len` to a positive number to generate only for a fraction of data.  
+    - Setting `data_frac` to be 0, 1, 2... to generate for different fractions of length `frac_len`.
+    - Note: maintain the same frac length when doing generation using data_frac. It's recommended to set a smaller `frac_len` to 800.
+
+The generated data is in json format where each data contains the following attributes:
+```
+{
+    "chosen": [{"role": "user", "content": <prompt>}, 
+               {"role": "assistant", "content": <ground truth>}],
+    "rejected": [{"role": "user", "content": <prompt>}, 
+                 {"role": "assistant", "content": <generation>}]
+}
+```
+
+#### Examples
+```
+accelerate launch generate.py --model alignment-handbook/zephyr-7b-sft-full --frac_len 800 --data_frac 0
+accelerate launch generate.py --model alignment-handbook/zephyr-7b-sft-full --frac_len 800 --data_frac 1
+...
+``` 
+
+### Step 2: Fine-tuning
+```
+bash pipeline.sh
+```
+**[TODO]**: wrap up necessary codes into the folder spin. Add explainations/instructions here. 
+
+
+## Citation
+If you find this repo useful for your research, please consider citing the paper
+```
+@misc{chen2024selfplay,
+      title={Self-Play Fine-Tuning Converts Weak Language Models to Strong Language Models}, 
+      author={Zixiang Chen and Yihe Deng and Huizhuo Yuan and Kaixuan Ji and Quanquan Gu},
+      year={2024},
+      eprint={2401.01335},
+      archivePrefix={arXiv},
+      primaryClass={cs.LG}
+}
+```
---
+++ spin/generate.py
@@ -0,0 +1,106 @@
+from accelerate import Accelerator
+from accelerate.utils import gather_object
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch, time, json
+import argparse
+from datasets import load_dataset
+from tqdm import tqdm
+import warnings
+warnings.filterwarnings("ignore")
+import os
+from datetime import timedelta
+from accelerate.utils import InitProcessGroupKwargs
+
+kwargs = InitProcessGroupKwargs(timeout=timedelta(seconds=36000))
+accelerator = Accelerator(kwargs_handlers=[kwargs])
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=str, default='alignment-handbook/zephyr-7b-sft-full')
+parser.add_argument('--data_frac', type=int, default=0)
+parser.add_argument('--frac_len', type=int, default=0)
+parser.add_argument('--output_dir', type=str, default='ultrachat_multi_gpu')
+parser.add_argument('--batch_size', type=int, default=16)
+parser.add_argument('--input_dir', type=str, default='synthetic_ultra_14k')
+
+args = parser.parse_args()
+model_path = args.model
+data_frac = args.data_frac
+batch_size = args.batch_size
+
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
+
+# load a base model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(
+    model_path,    
+    device_map={"": accelerator.process_index},
+    torch_dtype=torch.bfloat16,
+)
+tokenizer = AutoTokenizer.from_pretrained(model_path)   
+tokenizer.pad_token = tokenizer.eos_token
+
+# load data
+data = load_dataset(args.input_dir, split='train')
+data = data.shuffle(seed=42)
+if args.frac_len > 0:
+    sub_len = args.frac_len 
+    data = data[sub_len*data_frac:sub_len*(data_frac+1)]['chosen']
+
+prompts_all = ["### Instruction: " + data[idx][0]['content'] + "\n\n### Response: " for idx in range(len(data))]
+prompts_old = [data[idx][0]['content'] for idx in range(len(data))]
+corrects_all = [data[idx][1]['content'] for idx in range(len(data))]
+
+# batch, left pad (for inference), and tokenize
+def prepare_prompts(prompts, tokenizer, batch_size=4):
+    batches=[prompts[i:i + batch_size] for i in range(0, len(prompts), batch_size)]  
+    batches_tok=[]
+    tokenizer.padding_side="left"     
+    for prompt_batch in batches:
+        batches_tok.append(
+            tokenizer(
+                prompt_batch, 
+                return_tensors="pt", 
+                padding='longest', 
+                truncation=False, 
+                pad_to_multiple_of=8,
+                add_special_tokens=False).to("cuda") 
+            )
+    tokenizer.padding_side="right"
+    return batches_tok
+
+# sync GPUs and start the timer
+accelerator.wait_for_everyone()    
+start=time.time()
+
+# divide the prompt list onto the available GPUs 
+with accelerator.split_between_processes(prompts_all) as prompts:
+    results = []
+
+    # have each GPU do inference in batches
+    prompt_batches=prepare_prompts(prompts, tokenizer, batch_size=args.batch_size)
+
+    for prompts_tokenized in tqdm(prompt_batches):
+        # set max_new_tokens smaller for faster inference
+        outputs_tokenized=model.generate(**prompts_tokenized, max_new_tokens=256, pad_token_id=tokenizer.eos_token_id)
+
+        # remove prompt from gen. tokens
+        outputs_tokenized=[ tok_out[len(tok_in):] 
+            for tok_in, tok_out in zip(prompts_tokenized["input_ids"], outputs_tokenized) ] 
+        # decode gen. tokens 
+        outputs=tokenizer.batch_decode(outputs_tokenized)
+        results.extend(outputs)
+
+# collect results from all the GPUs and remove paddings
+results_gathered=gather_object(results)
+results = [r.replace("</s>","").lstrip() for r in results_gathered]
+
+if accelerator.is_local_main_process:
+    timediff=time.time()-start
+    print(f"time elapsed: {timediff}")
+
+    # collecting data
+    for idx in range(len(corrects_all)):
+        d = {"chosen": [{"role": "user", "content": prompts_old[idx]}, {"role": "assistant", "content": corrects_all[idx]}], "rejected": [{"role": "user", "content": prompts_old[idx]}, {"role": "assistant", "content": results[idx]}]}
+        with open(f"{args.output_dir}/loser_{data_frac}.jsonl", 'a') as f:
+            json.dump(d, f)
+            f.write('\n')
